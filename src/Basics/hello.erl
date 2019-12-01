13c62edd844b4fa7227ae5c022b0d2992d83c984
James Dorrington <jdorrington@hotmail.com>
2019-12-01 23:42:55 +0000

    Minor updates for outputting binary, hex etc.

--- src/Basics/hello.erl
+++ src/Basics/hello.erl
@@ -8,7 +8,13 @@
 -export([hello_world/0, main/0]).
 
 hello_world() ->
-    io:fwrite("Hello, world!").
+    io:fwrite("Hello, world!~n").
     
 main() ->
-    hello_world().
+    hello_world(),
+    io:fwrite("2 + 3 = ~B~n", [2 + 3]),
+    io:fwrite("a float might be ~f~n", [2.3e3]),
+    io:fwrite("11111111 binary is ~B decimal~n", [2#11111111]),
+    io:fwrite("FA0B hex is ~B decimal~n", [16#ffa]),
+    io:fwrite("~B decimal is ~.16B in hex~n", [1234, 1234])
+.
