c59784a9e253343c053a013bd284e0ca4c9b0cbb
James Dorrington <jdorrington@hotmail.com>
2019-12-02 07:29:23 +0000

    updating records.erl with examples of record_info/2

--- src/Basics/records.erl
+++ src/Basics/records.erl
@@ -1,18 +1,21 @@
 % TO RUN....
 % cd("C:\\Users\\jdorr\\Desktop\\Dev\\ErlangDump\\src\\Basics\\").
-% c("records.erl").
+% c("records.erl"). records:main().
 
 -module(records).
                  
 -compile(export_all).
 
--record(customer, {name = "", balence = 0.00}).
+-record(customer, {name = "", balance = 0.00}).
 
 main() -> 
-    Sally = #customer{name="Sally Smith", balence=12.34},
-    Sally2 = Sally#customer{balence = 50},
-    io:fwrite("~p owes ~p\n", [Sally#customer.name, Sally#customer.balence]), 
-    io:fwrite("~p owes ~p\n", [Sally2#customer.name, Sally2#customer.balence])
+    Sally = #customer{name="Sally Smith", balance=12.34},
+    Sally2 = Sally#customer{balance = 50},
+    io:fwrite("~p owes ~p\n", [Sally#customer.name, Sally#customer.balance]), 
+    io:fwrite("~p owes ~p\n", [Sally2#customer.name, Sally2#customer.balance]),
+    
+    io:format("Record fields: ~p~n", [record_info(fields, customer)]),
+    io:format("Record size: ~p~n", [record_info(size, customer)])
     .
     
     
